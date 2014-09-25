8bbd44e34a436cb69581a3b7a05b70d451371b0e
Mainian <k032492@gmail.com>
2014-09-25 00:44:45 -0400

    Added things

--- EquationFinder/SolutionFinder.cs
+++ EquationFinder/SolutionFinder.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,10 +9,94 @@ namespace EquationFinder
 {
     public class SolutionFinder
     {
-        public List<EF_Equation> Equations { get; set; }
-        public Dictionary<char, dynamic> VariableValues { get; set; }
-        public int PopulationSize { get; set; }
-        public double MutationRate { get; set; }
-        public double Answer { get; set; }
+        private Dictionary<char, dynamic> inputs;
+        private dynamic output;
+        private dynamic acceptableCost;
+        private dynamic mutationRate;
+        private EF_Equation seed;
+        private int populationSize;
+
+        #region Properties
+        public List<EF_Equation> Equations
+        {
+            get;
+            private set;
+        }
+
+        public Dictionary<char, dynamic> VariableValues
+        {
+            get
+            {
+                return inputs;
+            }
+        }
+
+        public int PopulationSize
+        {
+            get
+            {
+                return populationSize;
+            }
+        }
+
+        public dynamic MutationRate
+        {
+            get
+            {
+                return mutationRate;
+            }
+        }
+
+        public dynamic AcceptableCost
+        {
+            get
+            {
+                return acceptableCost;
+            }
+        }
+
+        public dynamic Answer
+        {
+            get
+            {
+                return output;
+            }
+        }
+        #endregion
+
+        public SolutionFinder(Dictionary<char, dynamic> inputs, dynamic output, dynamic acceptableCost, dynamic mutationRate, int populationSize)
+        {
+            fillOut(inputs, output, acceptableCost, mutationRate, populationSize);
+        }
+
+        public SolutionFinder(Dictionary<char, dynamic> inputs, dynamic output, dynamic acceptableCost, dynamic mutationRate, int populationSize, EF_Equation seed)
+        {
+            fillOut(inputs, output, acceptableCost, mutationRate, populationSize);
+            this.seed = seed;
+        }
+
+        private void fillOut(Dictionary<char, dynamic> inputs, dynamic output, dynamic acceptableCost, dynamic mutationRate, int populationSize)
+        {
+            this.inputs = inputs;
+            this.output = output;
+            this.acceptableCost = acceptableCost;
+            this.mutationRate = mutationRate;
+            this.populationSize = populationSize;
+        }
+
+        private void initializePopulation()
+        {
+
+        }
+
+        public void PerformStep()
+        {
+        }
+
+        public void PerformSteps(int steps)
+        {
+        }
+
+
     }
 }
